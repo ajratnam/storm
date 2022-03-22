2ce16562d077a1b4a2a97f268f24112a1c624e90
Ajay Ratnam <ajayratnam.satheesh@gmail.com>
2022-03-22 23:54:48 +0530

    Custom String Object, executor Makes String Objects Now!!!!

--- storm/derived_types.py
+++ storm/derived_types.py
@@ -1,16 +1,26 @@
-from storm.converters import number
+from math import modf, copysign
+
+from storm.converters import *
 from storm.generic_types import *
 
 
 class Number(Addable, Subtractable, Multipliable, Dividable, Negatable, Positable, Object):
     value: float
 
-    def __init__(self, value: Any) -> None:
-        super().__init__(value)
+    def __init__(self, value: Any, convert: bool = False) -> None:
+        super().__init__(value, convert)
+
+    @property
+    def value(self) -> float | int:
+        return int(self._value) if self.is_integer() else self._value
+
+    @staticmethod
+    def _base_convert(value: Any) -> float:
+        return number(value)
 
     @staticmethod
     def _convert(value: Any) -> float:
-        return number(value)
+        return force_number(value)
 
     def __add__(self, other: Object) -> 'Number':
         return Number(self.value + self._convert(other.value))
@@ -36,16 +46,75 @@ class Number(Addable, Subtractable, Multipliable, Dividable, Negatable, Positabl
     def __rdiv__(self, other: Object) -> 'Number':
         return Number(self._convert(other.value) / self.value)
 
-    def __neg__(self):
+    def __neg__(self) -> 'Number':
         return Number(-self.value)
 
-    def __pos__(self):
+    def __pos__(self) -> 'Number':
         return self
 
-    @property
-    def is_integer(self):
-        return self.value.is_integer()
+    def is_integer(self) -> bool:
+        return self._value.is_integer()
+
+    def reciprocal(self):
+        return Number(1 / self.value)
 
     def __repr__(self) -> str:
-        value = str(self.value)
-        return value[:-2] if self.is_integer else value
+        return str(self.value)
+
+
+class String(Addable, Subtractable, Multipliable, Dividable, Negatable, Positable, Object):
+    value: str
+
+    def __init__(self, value: Any, convert: bool = False) -> None:
+        super().__init__(value, convert)
+
+    @staticmethod
+    def _base_convert(value: Any) -> str:
+        return string(value)
+
+    @staticmethod
+    def _convert(value: Any) -> str:
+        return force_string(value)
+
+    def __add__(self, other: Object) -> 'String':
+        return String(self.value + self._convert(other.value))
+
+    def __radd__(self, other: Object) -> 'String':
+        return String(self._convert(other.value) + self.value)
+
+    def __sub__(self, other: Object) -> 'String':
+        return String(self.value.replace(self._convert(other.value), ''))
+
+    def __rsub__(self, other: Object) -> 'String':
+        return String(self._convert(other.value).replace(self.value, ''))
+
+    def __mul__(self, other: Object) -> 'String':
+        decimal, integer = modf(Number(other.value, True).value)
+        value = self.value
+        if copysign(1, integer) < 0:
+            value = value[::-1]
+        decimal, integer = abs(decimal), int(abs(integer))
+        return String(value * integer + value * int(len(value) * decimal))
+
+    def __rmul__(self, other: Object) -> 'String':
+        decimal, integer = modf(Number(self.value, True).value)
+        value = self._convert(other.value)
+        if copysign(1, integer) < 0:
+            value = value[::-1]
+        decimal, integer = abs(decimal), int(abs(integer))
+        return String(value * integer + value * int(len(value) * decimal))
+
+    def __div__(self, other: Object) -> 'String':
+        return self * Number(other.value, True).reciprocal()
+
+    def __rdiv__(self, other: Object) -> 'String':
+        return String(other.value) * Number(self, True).reciprocal()
+
+    def __neg__(self) -> 'String':
+        return String(self.value.lower())
+
+    def __pos__(self) -> 'String':
+        return String(self.value.upper())
+
+    def __repr__(self) -> str:
+        return self.value
--- storm/executor.py
+++ storm/executor.py
@@ -4,7 +4,7 @@ from storm import tokens
 from storm.generic_types import Object
 from storm.handlers import handle_operation, handle_prefix
 from storm.tokenzier import tokenize
-from storm.derived_types import Number
+from storm.derived_types import Number, String
 from storm.utils import Paginator
 
 
@@ -29,5 +29,7 @@ class Executor(Paginator[tokens.Token]):
             case tokens.PrefixedType:
                 token: tokens.PrefixedToken
                 return handle_prefix(self.parse(token.value), token.prefix)
+            case tokens.StringType:
+                return String(token.value)
             case other:
                 raise ValueError(f'Execution for token type {other} not created!!')
